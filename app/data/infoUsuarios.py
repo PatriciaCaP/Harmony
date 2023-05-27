cfd9c88b61b449c66f601c5eaafa3a83af908c7f
pepe <oscar.novillo@gmail.com>
2023-05-27 09:28:18 +0200

    arreglos

--- app/data/infoUsuarios.py
+++ app/data/infoUsuarios.py
@@ -4,10 +4,10 @@ class Usuarios:
    def __init__(self, db):
       self.db = db
 
-def addUsuario(self, nombre,email,contraseña):
-     if nombre and email and contraseña:
-          cursor = self.db.cursor()
-          sql = "INSERT INTO usuarios (nombre,email,contraseña) VALUES (%s,%s,%s)"
-          data = (nombre,email,contraseña)
-          cursor.execute(sql,data)
-          self.db.commit()
+   def addUsuario(self, nombre,email,contraseña):
+      if nombre and email and contraseña:
+            cursor = self.db.cursor()
+            sql = "INSERT INTO usuarios (nombre,email,contraseña) VALUES (%s,%s,%s)"
+            data = (nombre,email,contraseña)
+            cursor.execute(sql,data)
+            self.db.commit()
